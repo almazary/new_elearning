c6395a843622f0d396ddebbdd5add9f40d8ed417
Almazari <almazary@gmail.com>
2016-09-03 14:06:17 +0700

    hilangkan alert jika tidak ada koneksi internet

--- assets/comp/jquery/app.js
+++ assets/comp/jquery/app.js
@@ -65,11 +65,8 @@
             url = $("#info-update-link").val();
             $.ajax({
                 type: "GET",
-                url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+                url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=20&callback=?&q=' + encodeURIComponent(url),
                 dataType: 'json',
-                error: function(){
-                    alert('Unable to load feed, Incorrect path or invalid feed');
-                },
                 success: function(xml){
                     values = xml.responseData.feed.entries;
                     var l = 1;
